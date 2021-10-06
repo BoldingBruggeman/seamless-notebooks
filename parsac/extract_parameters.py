550d38ca64c7bb39ec0a51ff58d4a4f0d99c78c5
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2021-10-06 12:58:01 +0200

    added extract_parameters.py

---
+++ parsac/extract_parameters.py
@@ -0,0 +1,19 @@
+import argparse
+import yaml
+
+parser = argparse.ArgumentParser()
+parser.add_argument('fabm_yaml')
+parser.add_argument('--scale', type=float, default=0.3)
+args = parser.parse_args()
+
+with open(args.fabm_yaml) as f:
+    y = yaml.load(f)
+
+for name, info in y['instances'].items():
+    for parameter, value in info.get('parameters', {}).items():
+        path = 'instances/%s/parameters/%s' % (name, parameter)
+        if isinstance(value, (float, int)) and not isinstance(value, bool):
+            xml = '<parameter file="fabm.yaml" variable="%s" minimum="%s" maximum="%s" />' % (path, (1 - args.scale) * value, (1 + args.scale) * value)
+            if isinstance(value, int):
+                xml = '<!-- %s -->' % xml
+            print(xml)
