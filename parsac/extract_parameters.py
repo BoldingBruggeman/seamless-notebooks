deb943f22186e387574006e304ca249b8787da49
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2021-10-06 16:05:05 +0200

    extract_parameters: swap min and max if appropriate

--- parsac/extract_parameters.py
+++ parsac/extract_parameters.py
@@ -16,7 +16,10 @@ for name, info in y['instances'].items():
     for parameter, value in parameters.items():
         path = 'instances/%s/parameters/%s' % (name, parameter)
         if isinstance(value, (float, int)) and not isinstance(value, bool):
-            xml = '<parameter file="fabm.yaml" variable="%s" minimum="%s" maximum="%s" />' % (path, (1 - args.scale) * value, (1 + args.scale) * value)
+            l, r = (1 - args.scale) * value, (1 + args.scale) * value
+            if r < l:
+                l, r = r, l
+            xml = '<parameter file="fabm.yaml" variable="%s" minimum="%s" maximum="%s" />' % (path, l, r)
             if isinstance(value, int):
                 xml = '<!-- %s -->' % xml
             print(xml)
